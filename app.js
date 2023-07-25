2b8c811899404f995058ff63b8e3fb7c6a257343
Kan5661 <linkan0718@gmail.com>
2023-07-25 13:53:38 -0400

    non working code

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let evaluatedNum, num1, num2, operationType
+let num1 = null
+let num2 = null
+let operationType = null
 
 // add event listener to the operation buttons (orange buttons)
 let operation_buttons = document.getElementsByClassName("operationButton")
 for (let i = 0; i < operation_buttons.length; i++) {
-    let OperationType = operation_buttons[i].innerHTML
-    operation_buttons[i].addEventListener('click', () => get_operation(OperationType))
+    let type = operation_buttons[i].innerHTML
+    operation_buttons[i].addEventListener('click', () => get_operation(type))
 }
 
 // add event listener to number buttons including decimal (grey buttons)
@@ -19,7 +21,6 @@ let clear_button = document.getElementById("clear")
 clear_button.addEventListener('click', clear)
 
 // add event listener to delete button
-
 let delete_button = document.getElementById("delete")
 delete_button.addEventListener('click', pop)
 
@@ -27,8 +28,36 @@ delete_button.addEventListener('click', pop)
 
 
 // the (fun)c stuffs
+function evaluate() {
+    let answer
+    if (operationType = '+') {
+        answer = num1 + num2
+    }
+    if (operationType = '–') {
+        answer = num1 - num2
+    }
+    return answer
+}
+
+
 function get_operation(type) {
+    value = Number(document.getElementsByClassName('evaluation')[0].innerText)
+    operationType = type
     console.log(type)
+    if (num1 != null && num2 != null && operationType != null) {
+        document.getElementsByClassName("evaluation")[0] = String(evaluate())
+        num2 = evaluate()
+        num1 = null
+    }
+    else if (num1 == null && num2 == null) { 
+        num1 = value
+        document.getElementsByClassName("evaluation")[0].innerHTML = ''
+    }
+    else if (num1 != null && num2 == null) { 
+        num2 = value
+    }
+    console.log(`num1 = ${num1} num2 = ${num2} operation: ${operationType}`)
+
 }
 
 function number_button(value) {
@@ -49,10 +78,13 @@ function change_display_value(concatString) {
 
 
 function clear() {
-    document.getElementsByClassName("evaluation")[0].innerText = ''
+    document.getElementsByClassName("evaluation")[0].innerHTML = ''
+    num1 = null
+    num2 = null
+    operationType = null
 }
 
 function pop() {
     value = document.getElementsByClassName("evaluation")[0]
-    value.innerText = value.innerText.slice(0, -1)
+    value.innerText = value.innerText.substring(0, value.innerText.length - 1)
 }
